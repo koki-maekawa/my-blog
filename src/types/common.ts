951e69312ce0c7904a48cf96384c48fc9a07e818
koki-maekawa <koki.study33@gmail.com>
2025-08-08 15:58:30 +0900

    chore: TIL関連型定義

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,22 @@
 export type IdParams = {params: Promise<{id: string}>};
 
+export type TagParams = {params: Promise<{tag: string}>};
+
+export type YearParams = {params: Promise<{year: string}>};
+
+export type MonthParams = {
+  params: Promise<{
+    year: string;
+    month: string;
+  }>;
+};
+
+export type DayParams = {
+  params: Promise<{
+    year: string;
+    month: string;
+    day: string;
+  }>;
+};
+
 export type SearchParams = {search?: string};
