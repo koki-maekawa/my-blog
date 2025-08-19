75ada52cff39f52cba8857d550c88e25d8301df7
koki-maekawa <koki.study33@gmail.com>
2025-08-19 23:44:20 +0900

    style: レイアウト変更

--- src/app/(public)/notions/page.tsx
+++ src/app/(public)/notions/page.tsx
@@ -14,18 +14,26 @@ export default async function notionsPage({
     ? await searchNotionPagesInfo(query)
     : await getNotionPagesInfo();
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl mb-12">学習メモ</h1>
-      <div className="flex justify-center my-6">
-        <NotionsSearch />
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {notionPagesInfo.map((notionPageInfo) => (
-          <NotionPageInfoCard
-            key={notionPageInfo.id}
-            notionPageInfo={notionPageInfo}
-          />
-        ))}
+    <div className="min-h-scren">
+      <div className="container mx-auto px-4 py-16">
+        <div className="text-center mb-16">
+          <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+            学習メモ
+          </h1>
+        </div>
+        <div className="max-w-3xl mx-auto">
+          <div className="flex justify-center mb-12">
+            <NotionsSearch />
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-6">
+            {notionPagesInfo.map((notionPageInfo) => (
+              <NotionPageInfoCard
+                key={notionPageInfo.id}
+                notionPageInfo={notionPageInfo}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
