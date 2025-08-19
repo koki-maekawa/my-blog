21c5d1ebcb2fcee05be599631af25cde9ca67cfc
koki-maekawa <koki.study33@gmail.com>
2025-08-19 23:36:27 +0900

    style: レイアウト変更

--- src/app/(public)/TIL/[year]/[month]/[day]/page.tsx
+++ src/app/(public)/TIL/[year]/[month]/[day]/page.tsx
@@ -1,4 +1,4 @@
-import {DayParams} from "@/types/common";
+import {DayParams} from "@/types/TIL";
 import {getTILDailyReport} from "@/lib/github";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -9,17 +9,23 @@ export default async function TILDayPage({params}: DayParams) {
   const dailyReport = await getTILDailyReport(year, month, day);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl mb-12">日報</h1>
-      <div className="max-w-3xl mx-auto bg-white border py-2">
-        <article className="prose prose-lg max-w-none p-8">
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeHighlight]}
-          >
-            {dailyReport}
-          </ReactMarkdown>
-        </article>
+    <div className="min-h-screen">
+      <div className="container mx-auto px-4 py-16">
+        <div className="text-center mb-16">
+          <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+            {`${year}年${month}月${day}日の学習記録`}
+          </h1>
+        </div>
+        <div className="max-w-4xl mx-auto">
+          <article className="prose prose-lg max-w-none p-8 md:p-12">
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              rehypePlugins={[rehypeHighlight]}
+            >
+              {dailyReport}
+            </ReactMarkdown>
+          </article>
+        </div>
       </div>
     </div>
   );
