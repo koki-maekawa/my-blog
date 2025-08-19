d9b7d68256f04ba88ccb6657535adace383e36cb
koki-maekawa <koki.study33@gmail.com>
2025-08-20 00:55:49 +0900

    fix: 型ガードの追加

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,14 @@
 import {clsx, type ClassValue} from "clsx";
 import {twMerge} from "tailwind-merge";
 import {pageNameMap, PageRoute} from "@/routes";
+import {Breadcrumb} from "@/types/common";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 // パンくずリスト作成
-export function generateBreadcrumbs(pathName: string) {
+export function generateBreadcrumbs(pathName: string): Breadcrumb[] {
   const arrPathName = pathName.split("/").filter((x) => x !== "");
   const breadcrumbPaths = [
     "/",
@@ -20,25 +21,25 @@ export function generateBreadcrumbs(pathName: string) {
     }),
   ];
 
-  return breadcrumbPaths
-    .map((path, index) => {
-      const currentPath = breadcrumbPaths[index];
+  const breadcrumbs = breadcrumbPaths.map((path, index) => {
+    const currentPath = breadcrumbPaths[index];
 
-      // 動的ルートと静的ルートの区別を行う
-      let key = path as PageRoute;
+    // 動的ルートと静的ルートの区別を行う
+    let key = path as PageRoute;
 
-      if (key.includes("/notions/tags/") && !pageNameMap[key]) {
-        key = PageRoute.NOTIONS_TAG;
-      } else if (key.includes("/notions/tags")) {
-        return null;
-      } else if (key.includes("/notions/") && !pageNameMap[key]) {
-        key = PageRoute.NOTION_DETAIL;
-      }
+    if (key.includes("/notions/tags/") && !pageNameMap[key]) {
+      key = PageRoute.NOTIONS_TAG;
+    } else if (key.includes("/notions/tags")) {
+      return null;
+    } else if (key.includes("/notions/") && !pageNameMap[key]) {
+      key = PageRoute.NOTION_DETAIL;
+    }
 
-      return {
-        path: currentPath,
-        label: pageNameMap[key] || currentPath.split("/").pop(),
-      };
-    })
-    .filter(Boolean);
+    return {
+      path: currentPath,
+      label: pageNameMap[key] || currentPath.split("/").pop(),
+    };
+  });
+
+  return breadcrumbs.filter((item): item is Breadcrumb => item !== null);
 }
--- src/types/common.ts
+++ src/types/common.ts
@@ -3,3 +3,8 @@ export type IdParams = {params: Promise<{id: string}>};
 export type TagParams = {params: Promise<{tag: string}>};
 
 export type SearchParams = {search?: string};
+
+export type Breadcrumb = {
+  path: string;
+  label: string | undefined;
+};
