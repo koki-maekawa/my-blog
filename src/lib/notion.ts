063e68f3e120966fdebe356f43f46d885b3a1ab5
koki-maekawa <koki.study33@gmail.com>
2025-09-03 12:12:12 +0900

    fix: インシデント応急処置の削除

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -14,31 +14,7 @@ export const notionHqClient = new Client({
 });
 
 // Notionページ用クライアント(react-notion-client)
-export const notionClient = new NotionAPI({
-  activeUser: process.env.NOTION_ACTIVE_USER,
-  authToken: process.env.NOTION_TOKEN_V2,
-  kyOptions: {
-    hooks: {
-      beforeRequest: [
-        (request, options) => {
-          const url = request.url.toString();
-
-          if (url.includes("/api/v3/syncRecordValues")) {
-            return new Request(
-              url.replace(
-                "/api/v3/syncRecordValues",
-                "/api/v3/syncRecordValuesMain"
-              ),
-              options
-            );
-          }
-
-          return request;
-        },
-      ],
-    },
-  },
-});
+export const notionClient = new NotionAPI();
 
 // NotionAPIレスポンスからページ情報抽出
 export function extractNotionPageInfo(post: PageObjectResponse) {
